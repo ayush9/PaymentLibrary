36308aa89269b851f539ce0f9aaba57849332462
ayushagarwal <ayush.agrawal@telekom-digital.com>
2025-02-01 22:22:30 +0530

    key modified

--- server.js
+++ server.js
@@ -1,4 +1,4 @@
-const stripe = require('stripe')(process.env.STRIPE_SECRET_KEY);
+const stripe = require('stripe')('sk_test_51QndE0GaLbzsMnocN6aOruHa3LOV2oa9bDkYIKCcd0h0T5i7bCnRWFlo9zfIIBPkG60zzxlImCNUaj6ezmOeCRgm00REaU9ovy');
 const express = require('express');
 const app = express();
 app.use(express.json());
