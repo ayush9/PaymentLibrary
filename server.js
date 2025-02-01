bc1238e291d1f53602097dc1fb4fec21c02c9aa7
ayushagarwal <ayush.agrawal@telekom-digital.com>
2025-02-01 22:02:23 +0530

    code modified

--- server.js
+++ server.js
@@ -1,69 +1,26 @@
-'use strict'
+require("dotenv").config();
+const express = require("express");
+const Stripe = require("stripe");
 
-var express = require('express')
-var app = express()
+const app = express();
+const stripe = Stripe(process.env.STRIPE_SECRET_KEY); // Secret key from .env
 
-app.set("port", process.env.PORT || 4000)
+app.use(express.json());
 
-app.get('/', function (req, res) {
-  console.log('GET request received')
-  res.writeHead(200, {'Content-Type': 'application/json'})
-  var response = { "response" : "This is GET method." }
-  console.log(response)
-  res.end(JSON.stringify(response))
-})
+app.post("/create-payment-intent", async (req, res) => {
+    try {
+        const { amount, currency } = req.body;
 
-app.get('/:id', function (req, res) {
-  console.log('GET /:id request received')
-  res.writeHead(200, {'Content-Type': 'application/json'})
-  var response = { "response" : "This is GET method with id=" + req.params.id + "." }
-  console.log(response)
-  res.end(JSON.stringify(response))
-})
+        const paymentIntent = await stripe.paymentIntents.create({
+            amount,
+            currency,
+        });
 
-app.post('/', function (req, res) {
-  console.log('POST request received')
-  res.writeHead(200, {'Content-Type': 'application/json'})
-  var response = { "response" : "This is POST method." }
-  console.log(response)
-  res.end(JSON.stringify(response))
-})
+        res.json({ clientSecret: paymentIntent.client_secret });
+    } catch (error) {
+        res.status(500).json({ error: error.message });
+    }
+});
 
-app.put('/', function (req, res) {
-  console.log('PUT request received')
-  res.writeHead(200, {'Content-Type': 'application/json'})
-  var response = { "response" : "This is PUT method." }
-  console.log(response)
-  res.end(JSON.stringify(response))
-})
-
-app.delete('/', function (req, res) {
-  console.log('DELETE request received')
-  res.writeHead(200, {'Content-Type': 'application/json'})
-  var response = { "response" : "This is DELETE method." }
-  console.log(response)
-  res.end(JSON.stringify(response))
-})
-
-const server = app.listen(app.get("port"), function () {
-  const host = server.address().address
-  const port = server.address().port
-
-  console.log("Node.js API app running at http://%s:%s", host, port)
-})
-
-process.on('SIGINT', () => {
-  console.log('Received SIGINT. Closing server...')
-  server.close(() => {
-    console.log('Server closed. Exiting process...')
-    process.exit(0)
-  })
-})
-
-process.on('SIGTERM', () => {
-  console.log('Received SIGTERM. Closing server...')
-  server.close(() => {
-    console.log('Server closed. Exiting process...')
-    process.exit(0)
-  })
-})
+const PORT = process.env.PORT || 3000;
+app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
